fb4b6f5802eccfd9ec46dbe69f2ca017e8e5672b
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-06-16 21:11:50 +0530

    Add files via upload

---
+++ Triangle_Area_Calculator/script.js
@@ -0,0 +1,5 @@
+function calculate(){
+    let b = parseFloat(document.getElementById("ab").value);
+    let h = parseFloat(document.getElementById("h").value);
+    document.getElementById("area").value = 0.5*b*h;
+}
